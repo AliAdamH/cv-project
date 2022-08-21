d70ab1a7041dd813956a25c9ca91ffee2d7f007d
Ali Hassaine <ali.hs@outlook.fr>
2022-08-21 12:52:16 +0000

    Change to functional App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import General from './General';
 import Education from './Education';
 import Experience from './Experience';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <div className="container__title">CV Application</div>
-        <General />
-        <Education />
-        <Experience />
-      </div>
-    );
-  }
+function App(props) {
+  return (
+    <div className="container">
+      <div className="container__title">CV Application</div>
+      <General />
+      <Education />
+      <Experience />
+    </div>
+  );
 }
+
+export { App };
